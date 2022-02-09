ec0d54bbf0f97bd00b19ceaf5e26c31945fc305e
Tomasz Puchalski <tomaszpuchalski@macbook-pro-tomasz.home>
2022-02-09 23:49:59 +0100

    Code refactor and logic change

--- reward_project.js
+++ reward_project.js
@@ -2,51 +2,83 @@
 let rewards = 100; //Number of USDT to split
 let days = 2; // How long will the contract be active
 let tokens_locked = new Map(); // Map of address => number of tokens * purchase power
-let total_locked = 0; // number of total locked tokens * their purchase power
+let total_power_locked = 0; // number of total locked tokens * their purchase power
 let testDate = new Date(Date.now() + 60*60*24*1000); //some test date to check if getRewards works properly
 
-//SOME ERC INFO
+//SOME ERC INFO JUST TO SIMULATE
 class TokenHolder {
-tokens_balance; //balance of tokens stored here
-purchase_power; // multiplier which indicates how much more will a certain address get
-address;
-last_payment;
+  tokens_balance; //balance of tokens stored here
+  address_power; // multiplier which indicates how much more will a certain address get
+  address;
+  last_payment;
+  rewards;
 
-constructor(address) {
-  this.tokens_balance = 0;
-  this.purchase_power = 1.0;
-  this.address = address;
+  constructor(address) {
+    this.tokens_balance = 0;
+    this.address_power = 1.0;
+    this.address = address;
+    this.rewards = 0;
+  }
 }
+
+//sends rewards to address
+function getRewards(TokenHolder) {
+  if(hasAtLeastOneDayPassed(TokenHolder)) {
+    returnTokens(TokenHolder, getTokensLocked(TokenHolder));
+    console.log("You weren't eligible for a single payout. Returning tokens");
+  }
+  TokenHolder.rewards = calculateReward(TokenHolder);
+}
+
+//returns tokens to a set address
+function returnTokens(TokenHolder, amount) {
+  TokenHolder.tokens_balance += getTokensLocked(TokenHolder);
+  tokens_locked.set(TokenHolder.address, tokens_locked.get(TokenHolder.address) - (amount * TokenHolder.address_power));
+}
+
+function calculateReward(TokenHolder) {
+  let rewardsDays = Math.floor(((testDate.getTime() - Date.now()) / 1000 / 60 / 60 + 0.01) / 24); // FOR TEST PURPOSES ONLY. USE last_payment - date.now INSTEAD!!!!!
+  let amount_due = tokens_locked.get(address)/total_power_locked * rewards/days * rewardsDays;
+}
+
+function addToTokensLocked(TokenHolder, amount) {
+  tokens_locked += amount * TokenHolder.address_power;
+}
+
+function subtractFromTokensLocked(TokenHolder, amount) {
+   tokens_locked -= amount * TokenHolder.address_power;
+}
+
+function getTokensLocked(TokenHolder) {
+  return tokens_locked.get(TokenHolder.address) / TokenHolder.address_power;
+}
+
+function hasAtLeastOneDayPassed(TokenHolder) {
+ return  (Math.floor(((TokenHolder.last_payment.getTime() - Date.now()) / 1000 / 60 / 60) / 24) < 1);
 }
 
 function lockTokens(TokenHolder, amount) {
-  if(amount <= TokenHolder.tokens_balance){
-     tokens_locked.set(TokenHolder.address, amount * TokenHolder.purchase_power);
-     TokenHolder.tokens_balance -= amount;
-     total_locked += amount * TokenHolder.purchase_power;
-     TokenHolder.last_payment = Date.now();
-}}
+    getRewards(TokenHolder.address);
+    if(amount <= TokenHolder.tokens_balance){
+      tokens_locked.set(TokenHolder.address, amount * TokenHolder.address_power);
+      TokenHolder.tokens_balance -= amount;
+      addToTokensLocked(TokenHolder, amount);
+      TokenHolder.last_payment = Date.now();
+    } else
+        console.log("you don't have enough tokens!")
+}
 
-
-//TODO checks
 function unlockTokens(TokenHolder, amount) {
-  TokenHolder.tokens_balance += amount;
-  let temp = tokens_locked.get(TokenHolder.address);
-  temp -= amount * TokenHolder.purchase_power;
-  tokens_locked.set(TokenHolder.address, temp);
-  total_locked -= amount * purchase_power;
+  if(amount > getTokensLocked(TokenHolder))
+    console.log("you can't unlock this many tokens!");
+    else {
+      getRewards(TokenHolder.address);
+      returnTokens(TokenHolder, amount);
+      subtractFromTokensLocked(TokenHolder, amount);
+    }
 }
 
-function getRewards(address) {
-  let rewardsDays = Math.floor(((testDate.getTime() - Date.now()) / 1000 / 60 / 60 + 0.01)/24);
-  console.log(rewardsDays);
 
-  let amount_due = tokens_locked.get(address)/total_locked * rewards/days * rewardsDays;
-
-  console.log(address + "received " + amount_due);
-
-  return amount_due;
-}
 
 i1 = new TokenHolder();
 i2 = new TokenHolder();
@@ -56,15 +88,15 @@ i2.address = 'address2';
 
 i1.tokens_balance = 50;
 i2.tokens_balance = 50;
-i2.purchase_power = 1.05;
+i2.address_power = 1.05;
 
 lockTokens(i1, 50);
 lockTokens(i2, 50);
 
 console.log(tokens_locked);
 
-r1 = getRewards(i1.address);
-r2 = getRewards(i2.address);
+r1 = getRewards(i1);
+r2 = getRewards(i2);
 
 document.write(r1);
 
